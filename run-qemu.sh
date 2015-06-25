a6385c675cee65a5618fe15ff2ddc96c585677b7
Basheer Subei <basheersubei@gmail.com>
2015-06-25 16:22:26 -0500

    qemu-start alias now runs in background properly

--- run-qemu.sh
+++ run-qemu.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+if sudo echo "Starting ..."
+then
+qemu-system-x86_64 ~/qemu-ubuntu/ubuntu.img -m 2G -enable-kvm -net user,hostfwd=tcp::10022-:22 -net nic -nographic &
+echo "qemu VM started. Waiting..."
+sleep 5
+
+echo "attempting to connect through SSH..."
 # taken from http://unix.stackexchange.com/a/82602/120817
    n=0
    until [ $n -ge 15 ]
    do
       ssh basheersubei@localhost -p10022 && break
       n=$[$n+1]
+      echo "Retrying SSH after 3 seconds..."
       sleep 3
    done
+fi
